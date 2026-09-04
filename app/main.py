0974e35050591e87e90f8e3758de3552f0799981
Aurora Bondre <Malina.Bondre@endava.com>
2026-09-04 11:24:28 +0300

    calculator web interface

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from app.api.routes import router
 from app.database import create_tables
-
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create the database table when the application starts
 create_tables()
@@ -13,6 +13,11 @@ app = FastAPI(
     description="REST API for power, Fibonacci, and factorial calculations.",
     version="1.0.0",
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Add all API endpoints
 app.include_router(router)
